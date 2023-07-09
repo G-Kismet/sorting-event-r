079c0cdfc14c980c6bd5baf6182703e6fd841505
AJ Dalbany Garrett <ajdalbanygarrett@AJs-MacBook-Pro.lan>
2023-07-09 18:42:01 -0400

    componnents

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import SortingYoda from "./components/SortingYoda";
+
+
+
+function App() {
+  return (
+    <div>
+     <SortingYoda />
+    </div>
+  );
+}
+
+export default App;
